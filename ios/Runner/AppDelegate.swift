9001a411ee1b328afb48072afa5bc3916226274b
Rodney Shen <114166609+rodnnnney@users.noreply.github.com>
2024-06-23 15:13:47 -0400

    changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,16 @@
 import UIKit
 import Flutter
-import GoogleMaps // Import Google Maps
+import GoogleMaps
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GMSServices.provideAPIKey("")
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GMSServices.provideAPIKey("AIzaSyBECOwLTAN6PU4Aet1b2QLGIb3kRK8Xjew")
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
--- lib/common_layer/consts/api_key.dart
+++ lib/common_layer/consts/api_key.dart
@@ -1,4 +1,3 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final String API_KEY = dotenv.env['API_KEY']!;
-//AIzaSyCw23kDf2jJs7sUILVm2vk6oIki8n8zymY
