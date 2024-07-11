e69afba2a3dc52b6a97b4eb3408791d5211ffeff
Rodney Shen <114166609+rodnnnney@users.noreply.github.com>
2024-07-11 18:29:58 -0400

    popping discount card bug fixed

--- lib/presentation_layer/pages/discount_card.dart
+++ lib/presentation_layer/pages/discount_card.dart
@@ -9,10 +9,11 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 class DiscountCard extends ConsumerStatefulWidget {
   static String id = 'discount_card';
 
-  DiscountCard(
-      {required this.firstName,
-      required this.lastName,
-      required this.restName});
+  DiscountCard({
+    required this.firstName,
+    required this.lastName,
+    required this.restName,
+  });
 
   final String firstName;
   final String lastName;
@@ -38,115 +39,120 @@ class _DiscountCardState extends ConsumerState<DiscountCard> {
 
     var place = getInfo(restaurantProvider, widget.restName);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(place.restaurantName),
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: CachedNetworkImage(
-                    imageUrl: "https://acacpicturesgenerealbucket.s3.amazonaws"
-                        ".com/acac1.gif",
-                    repeat: ImageRepeat.repeat,
-                    fit: BoxFit.contain),
-              ),
-              Column(
-                children: [
-                  const SizedBox(height: 10),
-                  Card(
-                    elevation: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 15),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CircleAvatar(
-                            minRadius: 18,
-                            maxRadius: 24,
-                            backgroundImage:
-                                CachedNetworkImageProvider(place.imageLogo),
-                          ),
-                          const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'ACAC Discount',
-                                style: TextStyle(fontWeight: FontWeight.w600),
-                              ),
-                              Text('Valid until 08/31/2025')
-                            ],
-                          ),
-                          Container(
-                            width: 0.5,
-                            height: 48,
-                            color: Colors.black,
-                          ),
-                          const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '10%',
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color(0xffE68437)),
-                              ),
-                              Text(
-                                'OFF',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Color(0xffE68437),
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12),
-                child: Row(
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(place.restaurantName),
+          automaticallyImplyLeading:
+              false, // Optional: Prevents showing the back button
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: CachedNetworkImage(
+                      imageUrl:
+                          "https://acacpicturesgenerealbucket.s3.amazonaws"
+                          ".com/acac1.gif",
+                      repeat: ImageRepeat.repeat,
+                      fit: BoxFit.contain),
+                ),
+                Column(
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const Text(
-                          'Card Holder:',
-                          style: TextStyle(fontSize: 18),
-                          textAlign:
-                              TextAlign.left, // Aligns the text to the left
+                    const SizedBox(height: 10),
+                    Card(
+                      elevation: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              minRadius: 18,
+                              maxRadius: 24,
+                              backgroundImage:
+                                  CachedNetworkImageProvider(place.imageLogo),
+                            ),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'ACAC Discount',
+                                  style: TextStyle(fontWeight: FontWeight.w600),
+                                ),
+                                Text('Valid until 08/31/2025')
+                              ],
+                            ),
+                            Container(
+                              width: 0.5,
+                              height: 48,
+                              color: Colors.black,
+                            ),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '10%',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xffE68437)),
+                                ),
+                                Text(
+                                  'OFF',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Color(0xffE68437),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
                         ),
-                        Text(
-                          "${widget.firstName} ${widget.lastName}",
-                          style: const TextStyle(fontSize: 32),
-                          textAlign:
-                              TextAlign.left, // Aligns the text to the left
-                        )
-                      ],
+                      ),
                     ),
                   ],
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            'Card Holder:',
+                            style: TextStyle(fontSize: 18),
+                            textAlign:
+                                TextAlign.left, // Aligns the text to the left
+                          ),
+                          Text(
+                            "${widget.firstName} ${widget.lastName}",
+                            style: const TextStyle(fontSize: 32),
+                            textAlign:
+                                TextAlign.left, // Aligns the text to the left
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      bottomNavigationBar: AppBarBottom(
-        id: QRViewExample.id,
+        bottomNavigationBar: AppBarBottom(
+          id: QRViewExample.id,
+        ),
       ),
     );
   }
